29ceed1631b3779e7e74d0cd168b96c42386767b
Mervin Nguyen <mervin1@uci.edu>
2024-11-28 17:06:57 -0800

    Initial pseudo code to initialize flex sensors, servos, motors, and analog

---
+++ pseudocodeforSsytemValidation.cpp
@@ -0,0 +1,17 @@
+//System Validation Engineer: Mervin Nguyen
+//Company: Home
+
+//DescriptionL This is pseudocode for the system validation engineer position. 
+//This code will be used to validate the system of "T". 
+//The system will be validated by checking the system for any debugging. 
+
+//Pseudocode:
+//Using 5 servos for each finger on the hand. 
+
+//Loop position of sensors
+//initialize all flex sensors for each finger
+//initialize all servos for each finger
+//initialize all motors for each finger
+//initialize all sensors for each finger
+//read analog values from flex sensors
+
