65ebfe8cb11316e60f6aff461a983c9e2bfeffe2
Mervin Nguyen <mervin1@uci.edu>
2024-12-03 10:04:14 -0800

    Communicate using SPI protocol, servo libraries, and wireless communication

--- WirelessGlove.cpp
+++ WirelessGlove.cpp
@@ -1,2 +1,9 @@
 // Title: WIreless Glove Control
 //Description: Pseudocode for controlling a glove with flex sensors connected to an Arduino R3 and paired with a wireless trasnceiveer
+
+//libaries
+#include <Servo.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
